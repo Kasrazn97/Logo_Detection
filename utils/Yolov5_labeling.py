dd2fc3c97841dc9f74165b8507bb6ba9bde248dd
Kasra <74457464+Kasrazn97@users.noreply.github.com>
2021-10-18 15:21:31 +0200

    Added py files to prepare raw data for Yolov5

---
+++ utils/Yolov5_labeling.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import os
+
+os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
+asset_path = os.path.join(os.getcwd(), 'Assets')
+data_path = os.path.join(asset_path, 'DLCV_logo_project')
+train_path = os.path.join(data_path, 'train')
+mandatory_logos_path = os.path.join(train_path, 'mandatory_logos')
+optional_logos_path = os.path.join(train_path, 'optional_logos')
+noise_path = os.path.join(data_path, 'noise')
+
+
+def make_dir(path):
+    try:
+        os.mkdir(path)
+    except:
+        print('folder already exists')
+        pass
+    return
+
+def BndBox2YoloLine(box, noise=False,classList = []):
+
+    xmin = box['xmin']
+    xmax = box['xmax']
+    ymin = box['ymin']
+    ymax = box['ymax']
+
+    xcen = float((xmin + xmax)) / 2 / float(box['width'])
+    ycen = float((ymin + ymax)) / 2 / float(box['height'])
+
+    w = float((xmax - xmin)) / float(box['width'])
+    h = float((ymax - ymin)) / float(box['height'])
+
+    boxName = box['class']
+
+    try:
+        if noise:
+            classList.append('Null')
+            classIndex = -1
+        else:
+            if boxName not in classList:
+                classList.append(boxName)
+            classIndex = classList.index(boxName)
+    except:
+        print(box)
+
+    return classIndex, xcen, ycen, w, h
+
+
+def label_writer(file, list, path):
+    os.chdir(path)
+    with open(file, 'w') as f:
+        for i in list:
+            f.write(str(i))
+            f.write(' ')
+        f.close()
+
+def make_label(create_path,data_path,noise=False):
+    df = pd.read_csv(data_path)
+    for index, row in df.iterrows():
+        file_name = '{0}.txt'.format(row['photo_filename'])
+        if noise:
+            classIndex, xcen, ycen, w, h = BndBox2YoloLine(row,noise=True)
+        else:
+            classIndex, xcen, ycen, w, h = BndBox2YoloLine(row)
+        param_list = [classIndex, xcen, ycen, w, h]
+        label_writer(file_name, param_list,create_path)
+
+
+make_dir(os.path.join(mandatory_logos_path, 'images'))
+make_dir(os.path.join(mandatory_logos_path, 'labels'))
+
+make_dir(os.path.join(optional_logos_path, 'images'))
+make_dir(os.path.join(optional_logos_path, 'labels'))
+
+make_dir(os.path.join(noise_path, 'images'))
+make_dir(os.path.join(noise_path, 'labels'))
+
+print('making labels for mandatory logos')
+make_label(os.path.join(mandatory_logos_path,'labels'),os.path.join(data_path,'annotations_mandatory.csv'))
+print('making labels for optional logos')
+make_label(os.path.join(optional_logos_path,'labels'),os.path.join(data_path,'annotations_optional.csv'))
+print('making labels for noise images')
+make_label(os.path.join(noise_path,'labels'),os.path.join(data_path,'annotations_noise.csv'),noise=True)
--- utils/splitting_data.py
+++ utils/splitting_data.py
@@ -9,7 +9,10 @@ import os
 import shutil
 import re
 
-ROW_DATA_DIR = os.getcwd()
+os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
+asset_path = os.path.join(os.getcwd(),'Assets')
+data_path = os.path.join(asset_path,'DLCV_logo_project')
+os.chdir(data_path)
 
 annotations = pd.read_csv("annot_train.csv")
 annotations['class'] = annotations['class'].apply(lambda x: re.sub(' ', '_', x))
@@ -23,44 +26,55 @@ annotations.to_csv('annotations.csv', index=False)
 mandatory_classes = 'Nike, Adidas, Under_Armour, Puma, The_North_Face'.split(", ")
 annotations_mandatory = annotations[annotations['class'].apply(lambda x: x in mandatory_classes)]
 annotations_optional = annotations[annotations['class'].apply(lambda x: x not in mandatory_classes)]
+annotations_mandatory.to_csv('annotations_mandatory.csv',index=False)
+annotations_optional.to_csv('annotations_optional.csv',index=False)
 print(f'Mandatory annotations: {len(annotations_mandatory)}\nOptional annotations: {len(annotations_optional)}')
 
-train_data_dir = os.path.join(ROW_DATA_DIR, 'train')
+train_data_dir = os.path.join(data_path, 'train')
 train_pics = os.listdir(train_data_dir)
+
 
 # moving mandatory 5 logos to train/mandatory_logos folder
 mandatory_pics = [pic for pic in train_pics if pic in annotations_mandatory['photo_filename'].tolist()]
-os.mkdir(os.path.join(ROW_DATA_DIR, 'train/mandatory_logos'))
-mandatory_logos_dir = os.path.join(ROW_DATA_DIR, 'train/mandatory_logos')           
+os.mkdir(os.path.join(data_path, 'train','mandatory_logos','images'))
+mandatory_logos_dir = os.path.join(data_path, 'train','mandatory_logos','images')
 for pic in mandatory_pics:
      shutil.move(os.path.join(train_data_dir, pic), mandatory_logos_dir)
 
 # moving optional 12 logos to train/mandatory_logos folder
-optional_logos_dir = os.path.join(ROW_DATA_DIR, 'train/optional_logos')   
+optional_logos_dir = os.path.join(data_path, 'train','optional_logos','images')
 os.mkdir(optional_logos_dir)
 optional_pics = [pic for pic in train_pics if pic not in annotations_mandatory['photo_filename'].tolist()]
 for pic in optional_pics:
      shutil.move(os.path.join(train_data_dir, pic), optional_logos_dir)
 
+
 print(f'Number of mandatory 5 classes train pics: {len(os.listdir(mandatory_logos_dir))}\nNumber of optional 12 classes train pics: {len(os.listdir(optional_logos_dir))}')
+# To use in Roboflow you may uncomment the followings to devide into three batches due to it's limitation
+# batch1_dir = os.path.join(data_path,'train/mandatory_logos/batch1')
+# batch2_dir = os.path.join(data_path, 'train/mandatory_logos/batch2')
+# batch3_dir = os.path.join(data_path,'train/mandatory_logos/batch3')
+# os.mkdir(batch1_dir)
+# os.mkdir(batch2_dir)
+# os.mkdir(batch3_dir)
+#
+# for pic in [pic for pic in os.listdir(mandatory_logos_dir) if pic.endswith('jpg')][:7000]: # first 7000 pics
+#     shutil.move(os.path.join(mandatory_logos_dir, pic), batch1_dir)
+# for pic in [pic for pic in os.listdir(mandatory_logos_dir) if pic.endswith('jpg')][:7000]: # second 7000 pics
+#     shutil.move(os.path.join(mandatory_logos_dir, pic), batch2_dir)
+# for pic in [pic for pic in os.listdir(mandatory_logos_dir) if pic.endswith('jpg')]: # the rest
+#     shutil.move(os.path.join(mandatory_logos_dir, pic), batch3_dir, pic)
 
-batch1_dir = os.path.join(ROW_DATA_DIR,'train/mandatory_logos/batch1')
-batch2_dir = os.path.join(ROW_DATA_DIR, 'train/mandatory_logos/batch2')
-batch3_dir = os.path.join(ROW_DATA_DIR,'train/mandatory_logos/batch3')
-os.mkdir(batch1_dir)
-os.mkdir(batch2_dir)
-os.mkdir(batch3_dir)
-
-for pic in [pic for pic in os.listdir(mandatory_logos_dir) if pic.endswith('jpg')][:7000]: # first 7000 pics
-    shutil.move(os.path.join(mandatory_logos_dir, pic), batch1_dir)
-for pic in [pic for pic in os.listdir(mandatory_logos_dir) if pic.endswith('jpg')][:7000]: # second 7000 pics
-    shutil.move(os.path.join(mandatory_logos_dir, pic), batch2_dir)
-for pic in [pic for pic in os.listdir(mandatory_logos_dir) if pic.endswith('jpg')]: # the rest
-    shutil.move(os.path.join(mandatory_logos_dir, pic), batch3_dir, pic)
-
-print(f'Batch 1 size: {len(os.listdir(batch1_dir))}\nBatch 2 size: {len(os.listdir(batch2_dir))}\nBatch 3 size: {len(os.listdir(batch3_dir))}')
+# print(f'Batch 1 size: {len(os.listdir(batch1_dir))}\nBatch 2 size: {len(os.listdir(batch2_dir))}\nBatch 3 size: {len(os.listdir(batch3_dir))}')
 
 # renaming all noise classes to NULL
 annotations_noise = pd.read_csv("annot_noise.csv")
 annotations_noise['class'] = ['NULL']*len(annotations_noise)
 annotations_noise.to_csv('annotations_noise.csv', index=False)
+
+noise_logos_dir = os.path.join(data_path, 'noise','images')
+noise_dir = os.path.join(data_path, 'noise')
+os.mkdir(noise_logos_dir)
+nois_pics = os.listdir(noise_dir)
+for pic in nois_pics:
+     shutil.move(os.path.join(noise_dir, pic), noise_logos_dir)
