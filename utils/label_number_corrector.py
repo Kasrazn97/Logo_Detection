89aa45cdd43310c6cf6ddb69aebd9361a5958f07
Kasra <74457464+Kasrazn97@users.noreply.github.com>
2021-11-03 16:25:59 +0100

    usecase for nike_adidas images

--- utils/label_number_corrector.py
+++ utils/label_number_corrector.py
@@ -15,9 +15,7 @@ def main(opt):
         with in_place.InPlace(os.path.join(opt.inpDir, label)) as file:
             for line in file:
                 if int(line.split(' ', 1)[0]) == 1:
-                    line = '4' +' '+ line.split(' ', 1)[1]
-                elif int(line.split(' ', 1)[0]) == 17:
-                    line = '19' +' '+ line.split(' ', 1)[1]
+                    line = '6' +' '+ line.split(' ', 1)[1]
                 file.write(line)
 
 if __name__ == "__main__":
