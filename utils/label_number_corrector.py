7bd0d01f13e101f61bd0842f4014d1870e09e0a1
Kasra <74457464+Kasrazn97@users.noreply.github.com>
2021-11-03 15:58:20 +0100

    file to correct roboflow labels number

---
+++ utils/label_number_corrector.py
@@ -0,0 +1,25 @@
+import in_place
+import os
+import argparse
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inpDir', help='path to the labels')
+
+    opt = parser.parse_args()
+    return opt
+
+def main(opt):
+    for label in os.listdir(opt.inpDir):
+        with in_place.InPlace(os.path.join(opt.inpDir, label)) as file:
+            for line in file:
+                if int(line.split(' ', 1)[0]) == 1:
+                    line = '4' +' '+ line.split(' ', 1)[1]
+                elif int(line.split(' ', 1)[0]) == 17:
+                    line = '19' +' '+ line.split(' ', 1)[1]
+                file.write(line)
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
